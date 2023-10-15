be6fc5a2c32a2cfc3165785b8b58435b6cae00a3
smith <smithbabawande@gmail.com>
2023-10-15 07:51:06 +0100

    Completed Assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,13 @@
 def write_file(file_name, file_content):
-    pass
+    
+    file = open(f"{file_name}.txt", mode='w', encoding='utf-8')
+    file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    
+    file = open(f"{file_name}.txt", mode='a', encoding='utf-8')
+    file.write(append_content)
 
 def read_file(file_name):
-    pass
+    file = open(f"{file_name}.txt", mode='r', encoding='utf-8')
+    return file.read()
